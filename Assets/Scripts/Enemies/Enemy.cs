58e9000b2a68143201c763a444c781a10997cb0b
Batista-10 <tiagobatista6.gpsi@gmail.com>
2023-05-21 16:24:08 +0100

    enemy walking and detecting walls

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,123 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
 using UnityEngine;
 
-public abstract class Enemy : ScriptableObject
+public class Enemy : MonoBehaviour
 {
     //DATA
-    /*[SerializeField] protected EnemyData enemyData;
-    [SerializeField] protected ScriptableFloat playerHealth;
+    [SerializeField] public GameObject pointA;
+    [SerializeField] public GameObject pointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    public float mSpeed;
+    private Boolean facingRight;
 
-    //REFERENCES
-    [SerializeField] protected Transform touchCheck, projectileSpawn;
-    [SerializeField] protected LayerMask whatIsPlayer;
-    [SerializeField] protected GameObject projectilePf, deathBloodParticle;
-    //protected GameObject alive;
-
-    protected float currentHealth, lastShot, lastTouchDamageTime;
-    [SerializeField] protected float touchCheckRadius, touchDamageCooldown;
-    protected bool canShoot = true;
 
     protected virtual void Start()
     {
-        currentHealth = enemyData.MaxHealth;
-        //  alive = transform.Find("Alive").gameObject;
+        
     }
 
     protected virtual void Update()
     {
-        CheckPlayerHealth();
-        CheckAttack();
-        CheckTouchDamage();
+        
     }
-    protected virtual void CheckAttack()
-    {
-        if (CheckIfPlayerInRange())
-        {
-            //flip para o player
-            if (canShoot)
-            {
-                if (Time.time - lastShot < enemyData.Projectile.Cooldown)
-                {
-                    return;
-                }
-                lastShot = Time.time;
-
-                Instantiate(projectilePf, projectileSpawn.position, projectileSpawn.rotation);
-            }
-            else
-            {
-                //Debug.Log("CANT SHOOT");
-            }
-        }
-    }
-
-    protected virtual bool CheckIfPlayerInRange()
-    {
-
-        //criar uma caixa e ver se ta la o player
-        return true;
-    }
-    protected virtual void CheckPlayerHealth()
-    {
-        if (playerHealth.Value < enemyData.MinHealthToShoot)
-        {
-            canShoot = false;
-        }
-        else
-        {
-            canShoot = true;
-        }
-    }
-
-    public virtual void Damage(AttackDetails attackDetails)
-    {
-        currentHealth -= attackDetails.DamageAmount;
-        Debug.Log("damage to enemy");
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-    }
-
-    protected virtual void CheckTouchDamage()
-    {
-        if (Time.time >= lastTouchDamageTime + touchDamageCooldown)
-        {
-            AttackDetails attackDetails = new AttackDetails();
-            Collider2D hit = Physics2D.OverlapCircle(touchCheck.position, touchCheckRadius, whatIsPlayer);
-            //Debug.Log(hit);
-            if (hit != null && hit.tag == "Player")
-            {
-                //Debug.Log("damaged the player by " + enemyData.TouchDamage);
-
-                lastTouchDamageTime = Time.time;
-                attackDetails.DamageAmount = enemyData.TouchDamage;
-                attackDetails.AttackDirection = transform.position.x;
-                attackDetails.KnockbackAngle = enemyData.KnockbackForce;
-                attackDetails.KnockbackDuration = enemyData.TouchKnockbackDuration;
-                hit.SendMessage("Damage", attackDetails);
-
-            }
-        }
-    }
-
-    protected virtual void Die()
-    {
-        Debug.Log("ENEMY DIED");
-        Instantiate(deathBloodParticle, transform.position, deathBloodParticle.transform.rotation);
-        Destroy(gameObject);
-    }
-
-    protected virtual void Flip()
-    {
-        Debug.Log("flip");
-    }
-    protected virtual void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(touchCheck.position, touchCheckRadius);
-    }*/
-
-    public abstract void Atack();
-    public abstract void Die();
+    
 }
--- Assets/Scripts/Enemies/RangedEnemy.cs
+++ Assets/Scripts/Enemies/RangedEnemy.cs
@@ -1,18 +1,162 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class RangedEnemy : Enemy
+public class RangedEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*private Rigidbody2D rb;
+    
+    [SerializeField] private float jumpPower = 10.0f;
+    [SerializeField] private float movementSpeed = 5.0f;
+    [SerializeField] private float attackPower = 20.0f;*/
+
+    private enum State
+    {
+        Moving,
+        Attacking,
+        Dead
+    }
+
+    [SerializeField] private float groundCheckDistance, wallCheckDistance, mSpeed;
+    [SerializeField] private Transform groundCheck, wallCheck;
+    [SerializeField] private LayerMask whatIsGround;
+    
+    private State currentState;
+    private bool groundDetected, wallDetected;
+    private int facingDirection;
+    private GameObject alive;
+    private Rigidbody2D aliveRB;
+    private Animator anim;
+    private Vector2 movement;
+
+    private void Start()
+    {
+        alive = transform.Find("Alive").GameObject();
+        aliveRB = alive.GetComponent<Rigidbody2D>();
+        anim = alive.GetComponent<Animator>();
+        facingDirection = -1;
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case State.Moving:
+                UpdateMovingState();
+                break;
+            case State.Attacking:
+                UpdateAttackingState();
+                break;
+            case State.Dead:
+                UpdateDeadState();
+                break;
+        }
+    }
+    
+    //MOVING STATE------------------------------------------------------------------------
+    private void EnterMovingState()
     {
         
     }
+    
+    private void UpdateMovingState()
+    {
+        groundDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        wallDetected = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
 
-    // Update is called once per frame
-    void Update()
+        if (!groundDetected || wallDetected)
+        {
+            Flip();
+        }
+        else
+        {
+            //Move
+            movement.Set(mSpeed * facingDirection, aliveRB.velocity.y);
+            aliveRB.velocity = movement;
+        }
+    }
+    
+    private void ExitMovingState()
     {
         
+    }
+    
+    //ATTACKING STATE----------------------------------------------------------------------
+    private void EnterAttackingState()
+    {
+        
+    }
+    
+    private void UpdateAttackingState()
+    {
+        
+    }
+    
+    private void ExitAttackingState()
+    {
+        
+    }
+    
+    //DEAD STATE--------------------------------------------------------------------------
+    private void EnterDeadState()
+    {
+        Destroy(gameObject);
+    }
+    
+    private void UpdateDeadState()
+    {
+        
+    }
+    
+    private void ExitDeadState()
+    {
+        
+    }
+    
+    //OTHER FUNCTIONS--------------------------------------------------------------------
+    private void SwitchState(State state)
+    {
+        switch (currentState)
+        {
+            case State.Moving:
+                ExitMovingState();
+                break;
+            case State.Attacking:
+                ExitAttackingState();
+                break;
+            case State.Dead:
+                ExitDeadState();
+                break;
+        }
+        
+        switch (state)
+        {
+            case State.Moving:
+                EnterMovingState();
+                break;
+            case State.Attacking:
+                EnterAttackingState();
+                break;
+            case State.Dead:
+                EnterDeadState();
+                break;
+        }
+
+        currentState = state;
+    }
+
+    private void Flip()
+    {
+        facingDirection *= -1;
+        alive.transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector2(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector2(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
     }
 }
