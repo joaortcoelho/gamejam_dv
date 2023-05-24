1fd0f6ddc48d1125def8123ad5ea3628fbf7b549
Nuno Martinho <nuno.m.martinho@gmail.com>
2023-05-24 22:58:12 +0100

    enemy sounds, some corrections

--- Assets/Scripts/Enemies/EnemySpecific/MeleeEnemy/MeleeDamage.cs
+++ Assets/Scripts/Enemies/EnemySpecific/MeleeEnemy/MeleeDamage.cs
@@ -1,15 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MeleeDamage : MonoBehaviour
 {
+    private AudioSource audioSource;
+    [SerializeField] private ScriptableAudioClips hitSounds;
+    
     [SerializeField] private ScriptableFloat sanity;
     public float damage = 5f;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
+            AudioClip clip = hitSounds.Clips[UnityEngine.Random.Range(0, hitSounds.Clips.Length)];
+            audioSource.PlayOneShot(clip, 1f);
             sanity.Value -= damage;
             Debug.Log(sanity.Value);
         }
--- Assets/Scripts/Enemies/EnemySpecific/RangedEnemy/Projectile.cs
+++ Assets/Scripts/Enemies/EnemySpecific/RangedEnemy/Projectile.cs
@@ -10,12 +10,13 @@ public class Projectile : MonoBehaviour
     public Rigidbody2D rb;
     public float damage = 10f;
     [SerializeField] private ScriptableFloat sanity;
-    
+    private AudioSource audioSource;
+    [SerializeField] private ScriptableAudioClips hitSounds;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * -projectileSpeed;
-
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnEnable()
@@ -27,6 +28,8 @@ public class Projectile : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            AudioClip clip = hitSounds.Clips[UnityEngine.Random.Range(0, hitSounds.Clips.Length)];
+            audioSource.PlayOneShot(clip, 1f);
             sanity.Value -= damage;
             Debug.Log(sanity.Value);
         }
